d3cb4d14cdd27ccd3219c9364d01349f59674397
DevepMau <oshiettecs@gmail.com>
2026-03-13 19:47:53 -0300

    Refactoriza ControlesTabla simplificando la inicialización y agregando getters
    - Reduce campos y usa método para crear iconos
    - Elimina variables intermedias innecesarias
    - Añade métodos getter para los botones
    - Mejora la legibilidad y mantenimiento del código
    - Mantiene estilos y tooltips existentes
    - Usa constantes estáticas para tamaño de iconos

--- elSantiApp/src/com/elsantiapp/view/ControlesTabla.java
+++ elSantiApp/src/com/elsantiapp/view/ControlesTabla.java
@@ -8,78 +8,72 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 
 public class ControlesTabla extends HBox {
-	
-	private final int ICONO_ALTURA = 50;
-	
-	private HBox boxBotones;
-	private Button btnEliminar;
-	private Button btnModificar;
-	private Button btnAgregar;
-	private Button btnSwap;
-	private Image eliminar;
-	private Image modificar;
-	private Image agregar;
-	private Image swap;
-	private ImageView iconoEliminar;
-	private ImageView iconoModificar;
-	private ImageView iconoAgregar;
-	private ImageView iconoSwap;
-	private Tooltip ttEliminar;
-	private Tooltip ttModificar;
-	private Tooltip ttAgregar;
-	private Tooltip ttSwap;
-	
-	public ControlesTabla() {
-		
-		this.btnEliminar = new Button();
-		this.btnModificar = new Button();
-		this.btnAgregar = new Button();
-		this.btnSwap = new Button();
-		this.boxBotones = new HBox(2, btnSwap, btnEliminar, btnModificar, btnAgregar);
-		this.eliminar = new Image(getClass().getResource("/iconos/eliminar.png").toExternalForm());
-		this.modificar = new Image(getClass().getResource("/iconos/modificar.png").toExternalForm());
-		this.agregar = new Image(getClass().getResource("/iconos/agregar.png").toExternalForm());
-		this.swap = new Image(getClass().getResource("/iconos/swap.png").toExternalForm());
-		this.iconoEliminar = new ImageView(eliminar);
-		this.iconoModificar = new ImageView(modificar);
-		this.iconoAgregar = new ImageView(agregar);
-		this.iconoSwap = new ImageView(swap);
-		this.ttEliminar = new Tooltip("Eliminar");
-		this.ttModificar = new Tooltip("Modificar");
-		this.ttAgregar = new Tooltip("Agregar");
-		this.ttSwap = new Tooltip("Cambiar tabla");
-		
-		this.iconoEliminar.setFitHeight(ICONO_ALTURA);
-		this.iconoEliminar.setFitWidth(ICONO_ALTURA);
-		this.iconoModificar.setFitHeight(ICONO_ALTURA);
-		this.iconoModificar.setFitWidth(ICONO_ALTURA);
-		this.iconoAgregar.setFitHeight(ICONO_ALTURA);
-		this.iconoAgregar.setFitWidth(ICONO_ALTURA);
-		this.iconoSwap.setFitHeight(ICONO_ALTURA);
-		this.iconoSwap.setFitWidth(ICONO_ALTURA);
-		
-		btnEliminar.setGraphic(iconoEliminar);
-		btnModificar.setGraphic(iconoModificar);
-		btnAgregar.setGraphic(iconoAgregar);
-		btnSwap.setGraphic(iconoSwap);
-		
-		btnEliminar.setTooltip(ttEliminar);
-		btnModificar.setTooltip(ttModificar);
-		btnAgregar.setTooltip(ttAgregar);
-		btnSwap.setTooltip(ttSwap);
-		
-		this.setAlignment(Pos.CENTER_RIGHT);
-		
-		this.getChildren().add(boxBotones);
-		
-		this.getStylesheets().add(
-	    		getClass().getResource("/com/elsantiapp/css/controles-tabla.css").toExternalForm());
-		this.getStyleClass().add("controles-tabla-contenedor");
-		btnEliminar.getStyleClass().add("controles-tabla-boton");
-		btnModificar.getStyleClass().add("controles-tabla-boton");
-		btnAgregar.getStyleClass().add("controles-tabla-boton");
-		btnSwap.getStyleClass().add("controles-tabla-boton");
-	}
-	
 
+    private static final int ICONO_ALTURA = 50;
+
+    private Button btnEliminar;
+    private Button btnModificar;
+    private Button btnAgregar;
+    private Button btnSwap;
+
+    public ControlesTabla() {
+    	
+    	btnEliminar = new Button();
+        btnModificar = new Button();
+        btnAgregar = new Button();
+        btnSwap = new Button();
+
+        btnEliminar.setGraphic(crearIcono("/iconos/eliminar.png"));
+        btnModificar.setGraphic(crearIcono("/iconos/modificar.png"));
+        btnAgregar.setGraphic(crearIcono("/iconos/agregar.png"));
+        btnSwap.setGraphic(crearIcono("/iconos/swap.png"));
+
+        btnEliminar.setTooltip(new Tooltip("Eliminar"));
+        btnModificar.setTooltip(new Tooltip("Modificar"));
+        btnAgregar.setTooltip(new Tooltip("Agregar"));
+        btnSwap.setTooltip(new Tooltip("Cambiar tabla"));
+
+        HBox boxBotones = new HBox(2, btnSwap, btnEliminar, btnModificar, btnAgregar);
+
+        setAlignment(Pos.CENTER_RIGHT);
+        getChildren().add(boxBotones);
+
+        getStylesheets().add(
+            getClass().getResource("/com/elsantiapp/css/controles-tabla.css").toExternalForm()
+        );
+
+        getStyleClass().add("controles-tabla-contenedor");
+
+        btnEliminar.getStyleClass().add("controles-tabla-boton");
+        btnModificar.getStyleClass().add("controles-tabla-boton");
+        btnAgregar.getStyleClass().add("controles-tabla-boton");
+        btnSwap.getStyleClass().add("controles-tabla-boton");
+    }
+
+    private ImageView crearIcono(String ruta) {
+
+        Image img = new Image(getClass().getResource(ruta).toExternalForm());
+        ImageView icono = new ImageView(img);
+
+        icono.setFitHeight(ICONO_ALTURA);
+        icono.setFitWidth(ICONO_ALTURA);
+
+        return icono;
+    }
+
+	public Button getBtnEliminar() {
+		return btnEliminar;
+	}
+
+	public Button getBtnModificar() {
+		return btnModificar;
+	}
+
+	public Button getBtnAgregar() {
+		return btnAgregar;
+	}
+
+	public Button getBtnSwap() {
+		return btnSwap;
+	}
 }
