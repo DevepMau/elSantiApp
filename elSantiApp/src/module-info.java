89056ae1125c16ca4ff917ece68ee2a7a3d06fc1
DevepMau <oshiettecs@gmail.com>
2026-04-16 12:42:55 -0300

    Actualiza la base de datos y agrega javafx.base al módulo Java
    - Se actualizó el archivo binario datos.db
    - Se añadió la dependencia javafx.base en module-info.java
    - Se mantiene la apertura de com.elsantiapp.application
    - No se modificaron otras dependencias del módulo
    - Cambios compatibles con Java y JavaFX
    - Preparado para futuras funcionalidades

--- elSantiApp/src/module-info.java
+++ elSantiApp/src/module-info.java
@@ -2,6 +2,7 @@ module elSantiApp {
 	requires javafx.controls;
 	requires java.sql;
 	requires javafx.graphics;
+	requires javafx.base;
 	
 	opens com.elsantiapp.application to javafx.graphics, javafx.fxml;
 }
